5c1524da838825cca1d102b24364e275b86e2c2e
Eason Chiang <ishiu.chiang@gmail.com>
2019-08-31 18:25:20 +0800

    setup project description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/f2e-anonymous-chatroom",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Anonymous Chatroom`,
+    description: `劈哩啪啦的鍵盤聲，看似打程式很認真... | Designed by Miss_Y`,
+    author: `@EasonChiang7178`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `anonymous-chatroom`,
+        short_name: `chat`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1E1E1E`,
+        theme_color: `#D4D4D4`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/anonymous-chatroom-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
